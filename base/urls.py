762d2c100e823d3a5b088ec9976c6bb93e7e234a
Deaath65 <deaath65@gmail.com>
2023-07-09 11:09:06 +0545

    delete room

--- base/urls.py
+++ base/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'),
     path('create_room/', views.createRoom, name='create-room'),
     path('update_room/<str:pk>/', views.updateRoom, name='update-room'),
+    path('delete_room/<str:pk>/', views.deleteRoom, name='delete-room'),
 ]
