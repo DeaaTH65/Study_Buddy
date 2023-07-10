161d0e25ea8aa4e54c37a4ab5491dde7dd6accf1
Deaath65 <deaath65@gmail.com>
2023-07-10 14:00:51 +0545

    user register

--- base/views.py
+++ base/views.py
@@ -5,12 +5,14 @@ from .forms import RoomForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
@@ -30,13 +32,29 @@ def loginPage(request):
         else:
             messages.error(request, "Username of Password doesn't exist")
             
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
-def logoutPage(request):
+def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+            
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
