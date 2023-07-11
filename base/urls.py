318c63fff69d42fc3038a49659ab399f36d915e2
Deaath65 <deaath65@gmail.com>
2023-07-11 12:36:24 +0545

    user profile

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('create_room/', views.createRoom, name='create-room'),
     path('update_room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete_room/<str:pk>/', views.deleteRoom, name='delete-room'),
