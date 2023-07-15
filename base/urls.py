38cce7afece2ce42f1e7585a4f8a91b56ffee504
Deaath65 <deaath65@gmail.com>
2023-07-15 13:31:43 +0545

    mobile activity

--- base/urls.py
+++ base/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('delete_message/<str:pk>/', views.deleteMessage, name='delete-message'),
     path('update_user/', views.updateUser, name='update-user'),
     path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 ]
